5eebbe8cb3d674a2c66ffd8dbf4ae814c3474e30
Nektwork <animal_core@naver.com>
2021-08-17 00:43:07 +0900

    Initial commit

---
+++ src/main/java/net/nekcraft/ghosthunt/Ghosthunt.java
@@ -0,0 +1,16 @@
+package net.nekcraft.ghosthunt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Ghosthunt extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+}
---
+++ src/main/java/net/nekcraft/ghosthunt/techtest/skill/UseSkill.java
@@ -0,0 +1,14 @@
+package net.nekcraft.ghosthunt.techtest.skill;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class UseSkill {
+
+	@EventHandler
+	public void useSkill(PlayerInteractEvent evt) {
+
+
+
+	}
+}
